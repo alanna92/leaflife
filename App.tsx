93cb9b2ee0c9bbf7ba41e2753bb308fceb013db1
Alanna Vanzella <alanna92@gmail.com>
2020-07-29 23:13:36 -0300

    Refactoring

--- App.tsx
+++ App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialIcons } from '@expo/vector-icons';
-import { StatusBar } from 'expo-status-bar';
+import { View, StatusBar } from 'react-native';
+import Constants from 'expo-constants';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
-import Home from './pages/Home';
-import Profile from './pages/Profile';
+import Routes from './src/Routes';
 
-const Tab = createBottomTabNavigator();
-
-const MyTheme = {
+const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
@@ -19,55 +14,22 @@ const MyTheme = {
   },
 };
 
-const styles = StyleSheet.create({
-  statusBar: {
-    flex: 1,
-    backgroundColor: '#388E3C',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    paddingTop: 30,
-    maxHeight: 90,
-  },
-  statusBarText: {
-    color: '#fff',
-    fontSize: 24,
-    paddingLeft: 10,
-    fontWeight: 'bold',
-    letterSpacing: 2,
-  },
-});
-
 const App: React.FC = () => {
   return (
-    <NavigationContainer theme={MyTheme}>
-      <View style={styles.statusBar}>
-        <Text style={styles.statusBarText}>LeafLife</Text>
-        <StatusBar style="light" />
-      </View>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = 'home';
-            } else if (route.name === 'Profile') {
-              iconName = focused ? 'person' : 'person-outline';
-            }
-
-            // You can return any component that you like here!
-            return <MaterialIcons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'white',
-          inactiveTintColor: '#C8E6C9',
+    <PaperProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="rgba(0, 0, 0, 0.2)"
+        translucent
+      />
+      <View
+        style={{
+          backgroundColor: '#388E3C',
+          height: Constants.statusBarHeight,
         }}
-      >
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profile" component={Profile} />
-      </Tab.Navigator>
-    </NavigationContainer>
+      />
+      <Routes />
+    </PaperProvider>
   );
 };
 
---
+++ src/Routes.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+
+const Tab = createBottomTabNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#388E3C',
+    card: '#388E3C',
+  },
+};
+
+const Routes: React.FC = () => {
+  return (
+    <NavigationContainer theme={theme}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = '';
+
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'person' : 'person-outline';
+            }
+
+            // You can return any component that you like here!
+            return <MaterialIcons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'white',
+          inactiveTintColor: '#C8E6C9',
+        }}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
